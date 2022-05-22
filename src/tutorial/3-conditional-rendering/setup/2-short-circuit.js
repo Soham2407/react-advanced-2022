f8cad03a261fbbbf46e05f772bcfbf1a6b048d91
Soham2407 <nesarikarsoham24@gmail.com>
2022-05-22 11:35:09 +0530

    conditional rendering

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
+  const text = "";
+  const [isError, setIsError] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {text || <h2>hello world</h2>}
+      {isError && <h2>Error...</h2>}
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Error
+      </button>
+      <div style={{ marginTop: "4rem" }}>
+        {isLogin ? <h2>Welcome Soham</h2> : <h2>Please Login...</h2>}
+        <button className="btn" onClick={() => setIsLogin(!isLogin)}>
+          {isLogin ? "logout" : "login"}
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default ShortCircuit;
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [isLogIn, setIsLogIn] = useState(false);
+  const [isSignIn, setIsSignIn] = useState(false);
+  const user = "soham";
+  return (
+    <>
+      {!isSignIn && (
+        <button className="btn" onClick={() => setIsLogIn(!isLogIn)}>
+          Login
+        </button>
+      )}
+      {!isLogIn && (
+        <button className="btn" onClick={() => setIsSignIn(!isSignIn)}>
+          Signin
+        </button>
+      )}
+      <div style={{ marginTop: "2rem" }}>
+        {isLogIn && <Login user={user} />}
+        {isSignIn && <SignIn />}
+      </div>
+    </>
+  );
+};
+
+const Login = ({ user }) => {
+  return <h1>Helllo {user}</h1>;
+};
+
+const SignIn = () => {
+  return <h1>Please enter the fields</h1>;
 };
 
 export default ShowHide;
